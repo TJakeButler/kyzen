1437941bcb451c89d267f3a7fbd46a87051bec8d
Jake Butler <tjakebutler@gmail.com>
2021-03-06 12:35:25 -0600

    Added a small demo of an alert to be displayed when temp goes out of range

--- src/components/UpperAndLowerLimits/UpperAndLowerLimits.js
+++ src/components/UpperAndLowerLimits/UpperAndLowerLimits.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import { Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import { Button } from 'reactstrap';
+import { Alert } from 'reactstrap';
+import { UncontrolledAlert } from 'reactstrap';
+
+function AlertExample() {
+  return (
+    <UncontrolledAlert color="info">
+      I am an alert and I can be dismissed!
+    </UncontrolledAlert>
+  );
+}
+
+export default AlertExample;
 
 export const UpperAndLowerLimits = (props) => {
   return (
@@ -27,6 +39,7 @@ export const UpperAndLowerLimits = (props) => {
       </FormGroup>
       <Button color="primary">Celcius</Button>{' '}
       <Button color="success">Fahrenheit</Button>{' '}
+      <Button color="danger" onClick={() => alert('Warning Temp is out of Range!')}> Temp is Out of Range!!</Button>{' '}
       </Form>
   );
 }
